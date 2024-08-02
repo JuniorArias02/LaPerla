75b049ce917caadf69088eafd4e7a2e15947570f
Junior Arias <158210736+JuniorArias02@users.noreply.github.com>
2024-08-02 15:09:23 -0500

    se corrigio la base de datos y se agrego nueva columna llamada 'cantida'

--- src/main/java/BD/la_perla.sql
+++ src/main/java/BD/la_perla.sql
@@ -1,268 +1,190 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2024 a las 19:51:57
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: la_perla
+-- ------------------------------------------------------
+-- Server version	9.0.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `la_perla`
+-- Table structure for table `cliente`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
-  `codigo` int(11) NOT NULL,
+  `codigo` int NOT NULL,
   `nombre` varchar(250) NOT NULL,
-  `telefono` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `telefono` varchar(20) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `cliente`
+-- Dumping data for table `cliente`
 --
 
-INSERT INTO `cliente` (`codigo`, `nombre`, `telefono`) VALUES
-(1000322243, 'Cliente C', '3203333333'),
-(1000511111, 'Cliente E', '3205555555'),
-(1002311104, 'Cliente D', '3204444444'),
-(1034422453, 'Cliente B', '3202222222'),
-(1093900012, 'Cliente A', '3201111111');
-
--- --------------------------------------------------------
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (0,'CLIENTE GENERAL','0'),(12,'yeferson SUEn','3133153114'),(123,'1232','123'),(124,'grt','12323'),(1232,'ye','12323'),(1000322243,'Cliente C','3203333333'),(1000511111,'Cliente E','3205555555'),(1002311104,'Cliente D','3204444444'),(1034422453,'Cliente B','3202222222'),(1093900012,'Cliente A','3201111111');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `detalle_venta`
+-- Table structure for table `detalle_venta`
 --
 
+DROP TABLE IF EXISTS `detalle_venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `detalle_venta` (
-  `codigo` int(11) NOT NULL,
-  `producto` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `codigo` int NOT NULL,
+  `producto` int NOT NULL,
+  `cantidad` int NOT NULL,
+  KEY `producto` (`producto`),
+  KEY `codigo` (`codigo`),
+  CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `ventas` (`codigo`),
+  CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`producto`) REFERENCES `productos` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `detalle_venta`
+-- Dumping data for table `detalle_venta`
 --
 
-INSERT INTO `detalle_venta` (`codigo`, `producto`) VALUES
-(2, 9),
-(2, 11),
-(2, 11),
-(2, 14),
-(3, 16),
-(4, 9),
-(4, 9),
-(4, 10),
-(4, 10),
-(4, 15),
-(4, 16);
-
--- --------------------------------------------------------
+LOCK TABLES `detalle_venta` WRITE;
+/*!40000 ALTER TABLE `detalle_venta` DISABLE KEYS */;
+INSERT INTO `detalle_venta` VALUES (2,9,0),(2,11,0),(2,11,0),(2,14,0),(3,16,0),(4,9,0),(4,9,0),(4,10,0),(4,10,0),(4,15,0),(4,16,0),(27,9,0),(28,9,2),(28,10,1),(29,9,1),(29,12,1);
+/*!40000 ALTER TABLE `detalle_venta` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Table structure for table `productos`
 --
 
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
-  `codigo` int(11) NOT NULL,
+  `codigo` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(250) NOT NULL,
-  `precio` int(30) NOT NULL,
+  `precio` int NOT NULL,
   `categoria` varchar(250) NOT NULL,
-  `proveedor` int(11) NOT NULL,
-  `stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `proveedor` int NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `proveedor` (`proveedor`),
+  CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=1235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `productos`
+-- Dumping data for table `productos`
 --
 
-INSERT INTO `productos` (`codigo`, `nombre`, `precio`, `categoria`, `proveedor`, `stock`) VALUES
-(9, 'Producto 1', 15000, 'Electrónicos', 1, 50),
-(10, 'Producto 2', 25000, 'Electrodomésticos', 2, 100),
-(11, 'Producto 3', 35000, 'Ropa', 3, 75),
-(12, 'Producto 4', 45000, 'Hogar', 4, 30),
-(13, 'Producto 5', 55000, 'Electrónicos', 5, 60),
-(14, 'Producto 6', 65000, 'Electrodomésticos', 1, 25),
-(15, 'Producto 7', 75000, 'Ropa', 2, 80),
-(16, 'Producto 8', 85000, 'Hogar', 3, 40);
-
--- --------------------------------------------------------
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (9,'Producto 11',15000,'Electrónicos',1,50),(10,'Producto 2',25000,'Electrodomésticos',2,100),(11,'Producto 3',35000,'Ropa',3,75),(12,'Producto 4',45000,'Hogar',4,30),(13,'Producto 5',55000,'Electrónicos',5,60),(14,'Producto 6',65000,'Electrodomésticos',1,25),(15,'Producto 7',75000,'Ropa',2,80),(16,'Producto 8',85000,'Hogar',3,40),(1234,'int',1000,'int',1234,100);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `proveedor`
+-- Table structure for table `proveedor`
 --
 
+DROP TABLE IF EXISTS `proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `proveedor` (
-  `codigo` int(11) NOT NULL,
+  `codigo` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(250) NOT NULL,
-  `telefono` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `telefono` varchar(20) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=1235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `proveedor`
+-- Dumping data for table `proveedor`
 --
 
-INSERT INTO `proveedor` (`codigo`, `nombre`, `telefono`) VALUES
-(1, 'Proveedor A', '3201234567'),
-(2, 'Proveedor B', '3202345678'),
-(3, 'Proveedor C', '3203456789'),
-(4, 'Proveedor D', '3204567890'),
-(5, 'Proveedor E', '3205678901');
-
--- --------------------------------------------------------
+LOCK TABLES `proveedor` WRITE;
+/*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
+INSERT INTO `proveedor` VALUES (1,'Proveedor A','3201234567'),(2,'Proveedor B','3202345678'),(3,'Proveedor C','3203456789'),(4,'Proveedor D','3204567890'),(5,'Proveedor E','3205678901'),(12,'123','122'),(123,'tef','3'),(1234,'tefere','121313');
+/*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Table structure for table `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
   `usuario` varchar(30) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `gmail` varchar(250) NOT NULL,
-  `clave` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `clave` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Dumping data for table `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `usuario`, `nombre`, `gmail`, `clave`) VALUES
-(1, 'admi', 'administrador', 'administrador@gmail.com', '12345');
-
--- --------------------------------------------------------
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admi','administrador','administrador@gmail.com','12345');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `ventas`
+-- Table structure for table `ventas`
 --
 
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ventas` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
-  `cliente` int(11) DEFAULT NULL,
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cliente` int DEFAULT NULL,
   `monto` double NOT NULL,
-  `estado` enum('Fiado','Pago') NOT NULL DEFAULT 'Pago'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  PRIMARY KEY (`codigo`),
+  KEY `cliente` (`cliente`),
+  CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `ventas`
+-- Dumping data for table `ventas`
 --
 
-INSERT INTO `ventas` (`codigo`, `fecha`, `cliente`, `monto`, `estado`) VALUES
-(2, '2024-05-15 17:17:01', NULL, 150000, 'Pago'),
-(3, '2024-05-15 17:21:08', 1093900012, 85000, 'Fiado'),
-(4, '2024-05-15 17:22:50', 1034422453, 215000, 'Fiado');
+LOCK TABLES `ventas` WRITE;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+INSERT INTO `ventas` VALUES (2,'2024-05-15 22:17:01',NULL,150000),(3,'2024-05-15 22:21:08',1093900012,85000),(4,'2024-05-15 22:22:50',1034422453,215000),(5,'2024-07-22 12:03:50',0,-1666),(6,'2024-07-22 12:06:28',0,-23158),(7,'2024-07-22 12:10:23',0,-33091),(8,'2024-07-22 12:15:34',0,-127125),(9,'2024-07-22 12:26:16',0,-65082303),(10,'2024-07-22 12:27:19',0,11186),(11,'2024-07-22 12:27:23',0,11186),(12,'2024-07-22 12:27:23',0,11186),(13,'2024-07-22 12:27:23',0,11186),(14,'2024-07-22 12:27:24',0,11186),(15,'2024-07-22 12:27:24',0,11186),(16,'2024-07-22 12:27:24',0,11186),(17,'2024-07-22 12:27:24',0,11186),(23,'2024-07-31 19:42:13',0,13413413),(24,'2024-07-31 19:57:36',0,-226),(25,'2024-07-31 20:37:20',0,-225),(26,'2024-07-31 20:37:44',12,-136),(27,'2024-07-31 21:05:08',12,13893),(28,'2024-07-31 21:10:47',12,53630),(29,'2024-07-31 21:18:27',12,-1310135);
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- Indices de la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  ADD KEY `producto` (`producto`),
-  ADD KEY `codigo` (`codigo`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`codigo`),
-  ADD KEY `proveedor` (`proveedor`);
-
---
--- Indices de la tabla `proveedor`
---
-ALTER TABLE `proveedor`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- Indices de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`codigo`),
-  ADD KEY `cliente` (`cliente`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT de la tabla `proveedor`
---
-ALTER TABLE `proveedor`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `ventas` (`codigo`),
-  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`producto`) REFERENCES `productos` (`codigo`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`codigo`);
-
---
--- Filtros para la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`codigo`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-02 15:06:31
